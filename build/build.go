1b18838ef27b635b62d776e1da7c616caaa2a679
Edede Oiwoh <eoiwoh@gmail.com>
2023-07-10 12:16:11 -0400

    feat: pass build info to application

---
+++ build/build.go
@@ -0,0 +1,14 @@
+package build
+
+var (
+	// Version is the current version of the CLI
+	Version string
+	// Commit is the current commit of the CLI
+	Commit string
+	// Date is the date the CLI was built
+	Date string
+	// User is the user who built the CLI
+	User string
+	// BuiltBy is the machine that built the CLI
+	BuiltBy string
+)
--- cmd/root.go
+++ cmd/root.go
@@ -4,16 +4,44 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/ededejr/dotsh/build"
 	"github.com/spf13/cobra"
 )
 
+var rootCmdFlags struct {
+	Version   bool
+	BuildInfo bool
+}
+
 func init() {
+	rootCmd.Flags().BoolVarP(&rootCmdFlags.Version, "version", "v", false, "Get application version")
+	rootCmd.Flags().BoolVarP(&rootCmdFlags.BuildInfo, "build-info", "b", false, "Get application build info")
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 }
 
 var rootCmd = &cobra.Command{
 	Use:   "dotsh",
 	Short: "A CLI tool for generating and running executable scripts based on OpenAI chat completion",
+	Run: func(cmd *cobra.Command, args []string) {
+		hasBothFlagsPassed := rootCmdFlags.Version && rootCmdFlags.BuildInfo
+		hasNoFlagsPassed := !rootCmdFlags.Version && !rootCmdFlags.BuildInfo
+
+		if hasBothFlagsPassed || hasNoFlagsPassed {
+			cmd.Help()
+		}
+
+		if rootCmdFlags.Version {
+			fmt.Println(build.Version)
+		}
+
+		if rootCmdFlags.BuildInfo {
+			fmt.Println("Version:", build.Version)
+			fmt.Println("Commit:", build.Commit)
+			fmt.Println("Date:", build.Date)
+			fmt.Println("User:", build.User)
+			fmt.Println("Built by:", build.BuiltBy)
+		}
+	},
 }
 
 func Execute() {
